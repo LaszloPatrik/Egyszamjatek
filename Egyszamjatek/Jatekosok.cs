0d5a8ac08e613ad5818c74d38236b2134dfef756
LaszloPatrik <jericholicta@gmail.com>
2019-04-09 18:10:42 +0200

    Mama

---
+++ Egyszamjatek/Jatekosok.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Egyszamjatek
+{
+    class Jatekosok
+    {
+        public string nev;
+        public int[] tipps;
+
+        public Jatekosok(string sor)
+        {
+            var t = sor.Split(' ');
+
+            this.nev = t[0];
+            this.tipps = new int[t.Length - 1];
+
+            for (int i = 0; i < this.tipps.Length; i++)
+            {
+                tipps[i] = int.Parse(t[i + 1]);
+            }
+        }
+    }
+}
--- Egyszamjatek/Program.cs
+++ Egyszamjatek/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.IO;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +9,9 @@ namespace Egyszamjatek
 {
     class Program
     {
+        static List<Jatekosok> jatekos;
+        static int fordulo;
+
         static void Main(string[] args)
         {
             Beolvas();
@@ -20,22 +24,41 @@ namespace Egyszamjatek
 
         private static void Feladat05()
         {
-            
+            double ossz = 0;
+
+            for (int i = 0; i < jatekos.Count; i++)
+            {
+                ossz += jatekos[i].tipps[fordulo - 1];
+            }
+
+            double atlag = ossz / jatekos.Count;
+
+            Console.WriteLine("5. feladat: A megadott forduló tippjeinek átlaga: {0:F2} ", atlag);
+
         }
 
         private static void Feladat04()
         {
+            Console.Write("4. feladat: Kérem a forduló számát: ");
+            fordulo = int.Parse(Console.ReadLine());
             
         }
 
         private static void Feladat03()
         {
-            
+            Console.WriteLine("3. feladat: Játékosok száma: {0} fő", jatekos.Count);
         }
 
         private static void Beolvas()
         {
-            
+            jatekos = new List<Jatekosok>();
+
+            var sr = new StreamReader(@"egyszamjatek.txt", Encoding.UTF8);
+            while (!sr.EndOfStream)
+            {
+                jatekos.Add(new Jatekosok(sr.ReadLine()));
+            }
+            sr.Close();
         }
     }
 }
