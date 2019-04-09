3fbac09768795303f5ae540748ec7a5a8c4396ea
laszlo.patrik@vereb.dc <laszlo.patrik@vereb.dc>
2019-04-09 16:37:43 +0200

    Add project files.

---
+++ Egyszamjatek/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Egyszamjatek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Beolvas();
+            Feladat03();
+            Feladat04();
+            Feladat05();
+
+            Console.ReadKey();
+        }
+
+        private static void Feladat05()
+        {
+            
+        }
+
+        private static void Feladat04()
+        {
+            
+        }
+
+        private static void Feladat03()
+        {
+            
+        }
+
+        private static void Beolvas()
+        {
+            
+        }
+    }
+}
